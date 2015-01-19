75f136bf14737b2dd4d3247aa3d821a0059ba29f
ganna10 <janecoates10@gmail.com>
2015-01-19 16:39:14 +0100

    Added Mindmap to repository

---
+++ PhD_Thesis.mm
@@ -0,0 +1,36 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1408973950696" ID="ID_180927055" MODIFIED="1421681892174" TEXT="PhD Thesis">
+<node CREATED="1408973985731" ID="ID_430205592" MODIFIED="1408974576290" POSITION="right" TEXT="Summary + Zusammenfassung"/>
+<node CREATED="1408973993170" ID="ID_771388674" MODIFIED="1408973998539" POSITION="left" TEXT="Introduction"/>
+<node CREATED="1408974002346" ID="ID_779019666" MODIFIED="1408974008849" POSITION="right" TEXT="Methodology"/>
+<node CREATED="1408976075863" FOLDED="true" ID="ID_280447864" MODIFIED="1421681883511" POSITION="left" TEXT="Papers/Result Chapter/s">
+<node CREATED="1408976337608" ID="ID_900660818" MODIFIED="1408976344735" TEXT="Mechanism Comparison Study"/>
+<node CREATED="1410452545639" ID="ID_591181572" MODIFIED="1410452575788" TEXT="Systematic Testing of chemistry under different conditions">
+<node CREATED="1410452586385" ID="ID_1675369587" MODIFIED="1410452590985" TEXT="dilution"/>
+<node CREATED="1410452592173" ID="ID_1333707168" MODIFIED="1410452607626" TEXT="realistic NOx conditions"/>
+<node CREATED="1410452620873" ID="ID_1259096425" MODIFIED="1410452816003" TEXT="&apos;cloudiness&apos; by scaling photolysis rates"/>
+<node CREATED="1410452634257" ID="ID_75122200" MODIFIED="1410452643875" TEXT="NOx- to VOC-limited conditions"/>
+<node CREATED="1410452663708" ID="ID_1995965229" MODIFIED="1410452669742" TEXT="continuous VOC emissions"/>
+</node>
+<node CREATED="1408976345416" ID="ID_1587489538" MODIFIED="1421351901854" TEXT="Solvent Emissions"/>
+<node CREATED="1410452577930" ID="ID_382793880" MODIFIED="1410452582243" TEXT="OH Reactivity?"/>
+</node>
+<node CREATED="1408976081215" ID="ID_1311809358" MODIFIED="1408976084039" POSITION="right" TEXT="Outlook"/>
+<node CREATED="1408976099256" ID="ID_986275864" MODIFIED="1408976136077" POSITION="right" TEXT="Acknowledgments"/>
+<node CREATED="1408976324080" ID="ID_1790711696" MODIFIED="1408976331478" POSITION="left" TEXT="Discussion and Conclusions"/>
+<node CREATED="1408976374616" FOLDED="true" ID="ID_797229895" MODIFIED="1421681880911" POSITION="right" TEXT="Publication List">
+<node CREATED="1408976380224" ID="ID_1710278902" MODIFIED="1408976396406" TEXT="Scientific Articles"/>
+<node CREATED="1408976396832" ID="ID_551819404" MODIFIED="1408976400342" TEXT="Presentations">
+<node CREATED="1408976538160" ID="ID_1637693448" MODIFIED="1408978809794" TEXT="2013 AGU Fall Meeting, &quot;Comparing Chemical Mechanisms using Tagged Ozone Production Potentials&quot; 10th December 2013 Jane Coates and Tim Butler"/>
+<node CREATED="1408978679478" ID="ID_1850250986" MODIFIED="1408978832042" TEXT="2014 PhD Conference on Earth System Science &quot;Comparing how Chemical Mechanisms treat VOC Degradation and Impact on Ozone Production&quot; 13th March 2014 Jane Coates and Tim Butler"/>
+<node CREATED="1421351746158" ID="ID_1534026213" MODIFIED="1421351874911" TEXT="2014 OH Reactivity Specialists Uniting Meeting (ORSUM) &quot;VOC Source Attribution of OH, O3, and NO3 Reactivity&quot; 15th October 2014 Jane Coates and Tim Butler"/>
+</node>
+<node CREATED="1408976400984" ID="ID_934630364" MODIFIED="1408976403878" TEXT="Posters">
+<node CREATED="1408978679478" ID="ID_826143550" MODIFIED="1408978832042" TEXT="2014 PhD Conference on Earth System Science &quot;Comparing how Chemical Mechanisms treat VOC Degradation and Impact on Ozone Production&quot; 13th March 2014 Jane Coates and Tim Butler"/>
+<node CREATED="1408978860678" ID="ID_1876181356" MODIFIED="1408978971994" TEXT="2014 Our Climate Our Future &quot;Understanding Ozone Pollution: A Comparison of Chemical Mechanisms&quot; 7th October 2014 Jane Coates and Tim Butler"/>
+</node>
+</node>
+<node CREATED="1408976409928" ID="ID_969770340" MODIFIED="1408976413294" POSITION="left" TEXT="Appendix"/>
+</node>
+</map>
